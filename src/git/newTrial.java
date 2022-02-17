8f5337ac97cefb121dbddda2121ad1ed2def5b24
aircraftgraft <aircraftgraft@gmail.com>
2022-02-16 21:40:42 -0500

    updated code

--- src/git/newTrial.java
+++ src/git/newTrial.java
@@ -1,12 +1,12 @@
 package git;
 
-public class batch12 {
+public class newTrial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("new task");
 
-			System.out.println("Batch 12");
-			
 	}
 
 }
